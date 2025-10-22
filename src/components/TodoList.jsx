5d565178ab16ee01307b23f5c1229e6874fe6ca4
RameshKumawat <rameshkumawat5908@gmail.com>
2025-10-22 09:39:55 -0400

    Add empty state to TodoList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,6 +2,15 @@ import TodoItem from './TodoItem'
 import './TodoList.css'
  
 function TodoList({ todos, onToggle, onDelete, onEdit }) {
+  if (todos.length === 0) {
+    return (
+      <div className="empty-state">
+        <p className="empty-icon">📝</p>
+        <p className="empty-text">No tasks yet. Add one to get started!</p>
+      </div>
+    )
+  }
+
   return (
     <div className="todo-list">
       {todos.map(todo => (
